98b883d9da8f5ffe14602b3388cb39c03953a026
Murat Ay <murataybtp@gmail.com>
2018-11-19 21:28:00 +0300

    Ignores property when query builder runs

---
+++ Attributes/IgnoreAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperHelper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IgnoreAttribute : Attribute
+    {
+        //modelin içinde dbyi ilgilendirmeyen propertylere konularsa bunu atribute olarak ekleyinki sql querylerinde bulunmasın.
+    }
+}
