8b5a1ac9db8718b828ba83e80cc598dbb3584889
Murat Ay <murataybtp@gmail.com>
2018-11-19 21:24:09 +0300

    Show that this property is ID of table and dont include for update, use for update

---
+++ Attributes/DbKeyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperHelper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DbKeyAttribute : Attribute
+    {
+        //sql sorgusu üretirken buraya sahip propertyler insert sorgularında ignore edilecek
+        //update sorgusu içinse gerekicek
+    }
+}
