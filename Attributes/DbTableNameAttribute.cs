2b7b836b59d89546991dcab8045069fe91ee637b
Murat Ay <murataybtp@gmail.com>
2018-11-19 21:25:28 +0300

    Have two constructor property first table name second table alias for sql queries

---
+++ Attributes/DbTableNameAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperHelper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DbTableNameAttribute : Attribute
+    {
+        //Sorgular için tablo ismi ve aliasi
+        public string TableName { get; set; }
+        public string Alias { get; set; }
+
+        internal DbTableNameAttribute(string tableName, string alias)
+        {
+            TableName = tableName;
+            Alias = alias;
+        }
+    }
+}
