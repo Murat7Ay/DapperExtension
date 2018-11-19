a57685df968318783682dff441be6c0525ad5b1c
Murat Ay <murataybtp@gmail.com>
2018-11-19 21:22:02 +0300

    Base request to get consumer info and speciliazed keys

---
+++ BaseRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperHelper
+{
+    public class BaseRequest
+    {
+        public int CompanyId { get; set; }
+        public Dictionary<string, string> ConsumerKeys { get; set; }
+        public string ConsumerOrderTypeCode { get; set; }
+    }
+}
