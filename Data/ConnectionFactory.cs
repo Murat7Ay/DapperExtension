cec8c7f478f9abf4892aca9a382d379c635bd697
Murat Ay <murataybtp@gmail.com>
2018-11-19 21:09:30 +0300

    Static file to get open connection from sql

---
+++ Data/ConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperHelper.Data
+{
+    public class ConnectionFactory
+    {
+        /// <summary>
+        /// Dapper için sql ile bağlantı açar.
+        /// </summary>
+        /// <returns>Sql ile açık bağlantı döner. using() ile kullanmazsanız "Dispose" edin</returns>
+        public static DbConnection GetOpenConnection(string connectionString)
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
